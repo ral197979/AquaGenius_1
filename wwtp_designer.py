f09b1f9cc1da6d484febbeb8bb289a10c03e358b
ral197979 <ral34780@gmail.com>
2025-07-21 00:06:17 +0300

    Update wwtp_designer.py

--- wwtp_designer.py
+++ wwtp_designer.py
@@ -673,7 +673,7 @@ def generate_pfd_dot(inputs, sizing, results):
     return dot
 
 def generate_detailed_pdf_report(inputs, sizing, results):
-    pdf = FPDF()
+    pdf = PDF()
     pdf.add_page()
 
     pdf.chapter_title("1. Influent Design Criteria")
